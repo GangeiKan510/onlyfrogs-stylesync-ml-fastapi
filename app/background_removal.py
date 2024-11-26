b277c4667282c04cbbb78b0778a1567e96f99a73
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-11-26 21:50:47 +0800

    fix: preprocess images before uploading to firebase - openai ready images

--- app/background_removal.py
+++ app/background_removal.py
@@ -1,4 +1,7 @@
 # background_removal.py
+import cv2
+import numpy as np
+from PIL import Image, ImageEnhance
 import requests
 from fastapi import HTTPException
 from rembg import remove
@@ -7,6 +10,10 @@ from io import BytesIO
 from datetime import datetime
 import pyrebase
 from config import firebase_config  # Import firebase_config from config
+
+firebase = pyrebase.initialize_app(firebase_config)
+storage = firebase.storage()
+
 
 # Initialize Firebase with the imported config
 firebase = pyrebase.initialize_app(firebase_config)
@@ -22,6 +29,17 @@ async def remove_background(image_url: str):
 
         input_image = Image.open(BytesIO(response.content))
         output_image = remove(input_image)
+
+        output_image = output_image.resize((512, 512))
+
+        enhancer = ImageEnhance.Contrast(output_image)
+        output_image = enhancer.enhance(2)
+
+        output_image = output_image.convert("L")
+
+        output_image_np = np.array(output_image)
+        blurred_image = cv2.GaussianBlur(output_image_np, (5, 5), 0)
+        output_image = Image.fromarray(blurred_image)
 
         output_image_io = BytesIO()
         output_image.save(output_image_io, format='PNG')
